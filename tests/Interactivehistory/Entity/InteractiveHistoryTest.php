3af923e21eb554500e1b3353f19ada6a6c6c9234
Guilherme Siani <guilherme@siani.com.br>
2015-10-10 23:20:39 -0300

    Criado testes para retorno de conteúdo da história

--- tests/Interactivehistory/Entity/InteractiveHistoryTest.php
+++ tests/Interactivehistory/Entity/InteractiveHistoryTest.php
@@ -52,4 +52,55 @@ class InteractiveHistoryTest extends \PHPUnit_Framework_TestCase
 		$instance = new InteractiveHistory();
 		$instance->setContent(2);
 	}	
+
+	public function testGetContentShouldReturnAText()
+	{
+		$history = $this->getMockBuilder('History')->getMock();
+
+		$dilmasHistory = [];
+		$dilmasHistory[] = [
+			'Se hoje é o dia das crianças... Ontem eu disse: o dia da criança é o dia da mãe, 
+			dos pais, das professoras, mas também é o dia dos animais, sempre que você olha 
+			uma criança, há sempre uma figura oculta, que é um cachorro atrás. O que é algo 
+			muito importante!',
+			'Eu dou dinheiro pra minha filha. Eu dou dinheiro pra ela viajar, então é... é... 
+			Já vivi muito sem dinheiro, já vivi muito com dinheiro.'
+		];
+
+		$history->method('offsetGet')->will($this->onConsecutiveCalls(
+			$dilmasHistory[0][0],
+			$dilmasHistory[0][1]
+		));
+
+		$interactiveHistory = new InteractiveHistory($history);
+		$this->assertInternalType('string', $interactiveHistory->getContent(0, 0));
+		$this->assertEquals($dilmasHistory[0][0], $interactiveHistory->getContent(0, 0));
+		$this->assertInternalType('string', $interactiveHistory->getContent(0, 1));
+		$this->assertEquals($dilmasHistory[0][1], $interactiveHistory->getContent(0, 1));		
+	}
+
+	public function testGetUndefinedOffsetShouldReturnEmptyString()
+	{
+		$history = $this->getMockBuilder('History')->getMock();
+
+		$history->method('offsetExists')->will($this->onConsecutiveCalls(false));
+
+		$interactiveHistory = new InteractiveHistory($history);
+		$this->assertInternalType('string', $interactiveHistory->getContent(0, 0));
+		$this->assertEquals('', $interactiveHistory->getContent(0, 0));
+	}
+
+	public function testGetLastOffsetShouldReturnStringTheEnd()
+	{
+		$history = $this->getMockBuilder('History')->getMock();
+
+		$content = [];
+		$content[] = ['The end'];
+
+		$history->method('offsetGet')->will($this->onConsecutiveCalls($content[0][0]));
+
+		$interactiveHistory = new InteractiveHistory($history);
+		$this->assertInternalType('string', $interactiveHistory->getContent(0, 0));
+		$this->assertEquals($content[0][0], $interactiveHistory->getContent(0, 0));
+	}	
 }
