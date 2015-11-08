57e90603fbcbdb8259fed70ee66a988431cad83b
Guilherme Siani <guilherme@siani.com.br>
2015-11-08 00:43:17 -0200

    travis.yml

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -4,9 +4,4 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 date_default_timezone_get('America/Sao_Paulo');
 
-if (!@include __DIR__ . '/../vendor/autoload.php') {
-    die('You must set up the project dependencies, run the following commands:
-        wget http://getcomposer.org/composer.phar
-        php composer.phar install');
-}
-require $composer_autoload;
+include __DIR__ . "/../../vendor/autoload.php";
