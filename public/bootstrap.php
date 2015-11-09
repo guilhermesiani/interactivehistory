3dacc9d45764f0f573dcfb2b7fbf4607877a74c9
Guilherme Siani <guilherme@siani.com.br>
2015-11-08 23:21:40 -0200

    Criado estrutura de pasta para UI e inserido Bootstrap no pacote do composer

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -4,4 +4,11 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 date_default_timezone_get('America/Sao_Paulo');
 
-include __DIR__ . "/../../vendor/autoload.php";
+define('DS', DIRECTORY_SEPARATOR);
+define('APP_ROOT', realpath(__DIR__.DS.'..'));
+
+$composer_autoload = APP_ROOT.DS.'vendor'.DS.'autoload.php';
+if (!file_exists($composer_autoload)) {
+	die('Please, fucking install composer. http://getcomposer.org');
+}
+require $composer_autoload;
