61ba25166e6078a814f360fbef52488b5abe7ed3
Guilherme Siani <guilherme@siani.com.br>
2016-01-24 20:18:07 -0200

    config.php

--- config.php
+++ config.php
@@ -3,15 +3,15 @@
 date_default_timezone_set('America/Sao_Paulo');
 
 // Sempre use barra (/) no final das URLs
-define('URL', 'https://shrouded-shelf-67372.herokuapp.com/');
+define('URL', 'http://localhost:9090/');
 define('LIBS', 'libs/');
 
 // Configuração com Banco de Dados
 define('DB_TYPE', 'pgsql');
-define('DB_HOST', 'ec2-54-197-224-155.compute-1.amazonaws.com');
-define('DB_NAME', 'd9t6vfbngfu0r4');
-define('DB_USER', 'xswjdizupkmude');
-define('DB_PASS', 'srZ59DLILkk_SYyNaMAp1HeWB2');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'interactivehistory');
+define('DB_USER', 'guilhermesiani');
+define('DB_PASS', '');
 
 // HASH KEY, nunca mude esta parte, pois é usada para as senhas!
 define('HASH_GENERAL_KEY', 'MixitUp200');
@@ -26,3 +26,5 @@ define('IMG_SIZE', '1000000'); // Tamanho máximo do arquivo em bytes
 // Outros
 define('DS', DIRECTORY_SEPARATOR);
 define('APP_ROOT', realpath(__DIR__.DS));
+
+// https://shrouded-shelf-67372.herokuapp.com
